e58902490e65a149f27f0246f074f80f783c84ff
TheoQM <theo@quantum-machines.co>
2025-04-03 13:08:17 +0200

    update initialize_qpu

--- quam_builder/architecture/superconducting/qpu/base_quam.py
+++ quam_builder/architecture/superconducting/qpu/base_quam.py
@@ -179,5 +179,5 @@ class BaseQuAM(QuamRoot):
         Q_st = [declare_stream() for _ in range(len(self.qubits))]
         return I, I_st, Q, Q_st, n, n_st
 
-    def initialize_qpu(self):
+    def initialize_qpu(self, **kwargs):
         pass
