00edf15545ad7f5449da2947a955526be31a59a2
paulQM <paul.jamet@quantum-machines.co>
2025-07-18 12:27:38 +0200

    add cz macro for tunable transmons

--- quam_builder/architecture/superconducting/custom_gates/__init__.py
+++ quam_builder/architecture/superconducting/custom_gates/__init__.py
@@ -1,5 +1,6 @@
-from quam_builder.architecture.superconducting.custom_gates import cross_resonance
+from quam_builder.architecture.superconducting.custom_gates import cross_resonance, cz
 
 __all__ = [
     *cross_resonance.__all__,
+    *cz.__all__,
 ]
