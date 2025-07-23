7508dcaf66d0d4d2d204cf7f5b1afbbe01409bac
HiroQM <hiroyuki.inoue@quantum-machines.co>
2025-07-23 14:45:50 +0900

    resolved conflict and merged

--- quam_builder/architecture/superconducting/custom_gates/__init__.py
+++ quam_builder/architecture/superconducting/custom_gates/__init__.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-from quam_builder.architecture.superconducting.custom_gates import cross_resonance_gate
-
-__all__ = [
-    *cross_resonance_gate.__all__,
-=======
 from quam_builder.architecture.superconducting.custom_gates import cross_resonance, cz
 
 __all__ = [
     *cross_resonance.__all__,
     *cz.__all__,
->>>>>>> 00edf15545ad7f5449da2947a955526be31a59a2
 ]
