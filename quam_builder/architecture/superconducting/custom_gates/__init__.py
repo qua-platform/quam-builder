3faad30e585a2ee44c2763e98b4918c4778334a0
HiroQM <hiroyuki.inoue@quantum-machines.co>
2025-07-23 14:36:42 +0900

    renamed the module

--- quam_builder/architecture/superconducting/custom_gates/__init__.py
+++ quam_builder/architecture/superconducting/custom_gates/__init__.py
@@ -1,5 +1,5 @@
-from quam_builder.architecture.superconducting.custom_gates import cross_resonance
+from quam_builder.architecture.superconducting.custom_gates import cross_resonance_gate
 
 __all__ = [
-    *cross_resonance.__all__,
+    *cross_resonance_gate.__all__,
 ]
