d1f0ccd45cc504455b8752b3b9a2f09fbda37bea
paulQM <paul.jamet@quantum-machines.co>
2025-07-30 12:07:40 +0200

    remove active couplers

--- quam_builder/architecture/superconducting/qpu/base_quam.py
+++ quam_builder/architecture/superconducting/qpu/base_quam.py
@@ -59,7 +59,6 @@ class BaseQuam(QuamRoot):
     network: dict = field(default_factory=dict)
 
     active_qubit_names: List[str] = field(default_factory=list)
-    active_coupler_names: List[str] = field(default_factory=list)
     active_qubit_pair_names: List[str] = field(default_factory=list)
 
     ports: Union[FEMPortsContainer, OPXPlusPortsContainer] = None
