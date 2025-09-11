d3bd7fcb35ffacb1a813fce460fe0c179dc85a19
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-09-11 13:24:17 +0200

    cleanr offset parameter

--- quam_builder/architecture/quantum_dots/voltage_gate.py
+++ quam_builder/architecture/quantum_dots/voltage_gate.py
@@ -1,15 +1,19 @@
 from quam.components import SingleChannel
 from quam.core import quam_dataclass
-from typing import Optional
 
 
 @quam_dataclass
 class VoltageGate(SingleChannel):
     """
     A voltage gate is a single channel that can be used to apply a voltage to a quantum dot.
+
+    Attributes:
+        attenuation: The attenuation of the voltage gate. Default is zero
+        offset_parameter: The optional DC offset of the voltage gate
+            Can be e.g. a QDAC channel
     """
 
-    attenuation: Optional[float] = None
+    attenuation: float = 0.0
 
     def __post_init__(self):
         self._offset_parameter = None
