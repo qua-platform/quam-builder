7d4025964dddcaf4fba7fb8a9d8cc9399973d3d7
TheoQM <theo@quantum-machines.co>
2025-09-30 14:51:10 +0200

    replace `type() ==` by `isinstance`

--- quam_builder/builder/qop_connectivity/get_digital_outputs.py
+++ quam_builder/builder/qop_connectivity/get_digital_outputs.py
@@ -18,13 +18,13 @@ def get_digital_outputs(
     digital_outputs = dict()
     for i, item in enumerate([port for port in ports if "digital_output" in port]):
         if digital_marker_name == "octave_switch":
-            if type(DigitalOutputChannel(opx_output=f"{wiring_path}/{item}").opx_output) == FEMDigitalOutputPort:
+            if isinstance(DigitalOutputChannel(opx_output=f"{wiring_path}/{item}").opx_output, FEMDigitalOutputPort):
                 digital_outputs[f"{digital_marker_name}_{i}"] = DigitalOutputChannel(
                     opx_output=f"{wiring_path}/{item}",
                     delay=14,  # 14ns for QOP333 and above
                     buffer=13,  # 13ns for QOP333 and above
                 )
-            elif type(DigitalOutputChannel(opx_output=f"{wiring_path}/{item}").opx_output) == OPXPlusDigitalOutputPort:
+            elif isinstance(DigitalOutputChannel(opx_output=f"{wiring_path}/{item}").opx_output, OPXPlusDigitalOutputPort):
                 digital_outputs[f"{digital_marker_name}_{i}"] = DigitalOutputChannel(
                     opx_output=f"{wiring_path}/{item}",
                     delay=57,  # 57ns for QOP222 and above
