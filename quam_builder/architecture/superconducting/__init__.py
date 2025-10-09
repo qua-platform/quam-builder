b868cf703e42e46b001a9d8e6c68d0820d12d04a
paulQM <paul.jamet@quantum-machines.co>
2025-10-09 11:51:01 +0200

    remove CR macro

--- quam_builder/architecture/superconducting/__init__.py
+++ quam_builder/architecture/superconducting/__init__.py
@@ -1,17 +1,8 @@
-from .qpu import FixedFrequencyQuam, FluxTunableQuam, BaseQuam
-from .qubit import FixedFrequencyTransmon, FluxTunableTransmon, BaseTransmon
+from .components import cross_resonance, flux_line, mixer, readout_resonator, tunable_coupler, xy_drive, zz_drive
+from .custom_gates import CZGate
+from .qpu import BaseQuam, FixedFrequencyQuam, FluxTunableQuam
+from .qubit import BaseTransmon, FixedFrequencyTransmon, FluxTunableTransmon
 from .qubit_pair import FixedFrequencyTransmonPair, FluxTunableTransmonPair
-from .components import (
-    mixer,
-    readout_resonator,
-    flux_line,
-    tunable_coupler,
-    cross_resonance,
-    zz_drive,
-    xy_drive,
-)
-from .custom_gates.fixed_transmon_pair.two_qubit_gates import CRGate
-
 
 __all__ = [
     *qpu.__all__,
--- quam_builder/architecture/superconducting/custom_gates/__init__.py
+++ quam_builder/architecture/superconducting/custom_gates/__init__.py
@@ -1,7 +1,5 @@
-from quam_builder.architecture.superconducting.custom_gates.fixed_transmon_pair.two_qubit_gates import CRGate
 from quam_builder.architecture.superconducting.custom_gates.flux_tunable_transmon_pair.two_qubit_gates import CZGate
 
 __all__ = [
-    CRGate,
     CZGate,
 ]
