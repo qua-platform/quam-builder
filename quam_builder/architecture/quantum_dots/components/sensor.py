556f0b0f363ffefead84bb0dd55b05c0040d2127
KU-QM <kalid.ulas@quantum-machines.co>
2025-10-10 13:14:52 +0100

    Initial Skeleton

---
+++ quam_builder/architecture/quantum_dots/components/sensor.py
@@ -0,0 +1,12 @@
+
+from quam.core import quam_dataclass
+from voltage_gate import VoltageGate
+
+@quam_dataclass
+class Sensor(VoltageGate):
+    """
+    QuAM component for Sensor Quantum Dot 
+    """
+
+    pass
+
