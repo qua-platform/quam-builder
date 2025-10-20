179ffd74d296b4c8e099ec5125f38ff723d0d31b
HiroQM <hiroyuki.inoue@quantum-machines.co>
2025-10-20 11:45:36 +0900

    fix

--- quam_builder/architecture/superconducting/__init__.py
+++ quam_builder/architecture/superconducting/__init__.py
@@ -3,15 +3,6 @@ from .custom_gates import CZGate
 from .qpu import BaseQuam, FixedFrequencyQuam, FluxTunableQuam
 from .qubit import BaseTransmon, FixedFrequencyTransmon, FluxTunableTransmon
 from .qubit_pair import FixedFrequencyTransmonPair, FluxTunableTransmonPair
-from .components import (
-    mixer,
-    readout_resonator,
-    flux_line,
-    tunable_coupler,
-    cross_resonance,
-    zz_drive,
-    xy_drive,
-)
 
 __all__ = [
     *qpu.__all__,
--- quam_builder/architecture/superconducting/custom_gates/__init__.py
+++ quam_builder/architecture/superconducting/custom_gates/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from quam_builder.architecture.superconducting.custom_gates.fixed_transmon_pair.two_qubit_gates import CRGate, StarkInducedCZGate
 from quam_builder.architecture.superconducting.custom_gates.flux_tunable_transmon_pair.two_qubit_gates import CZGate
 
@@ -7,10 +6,3 @@ __all__ = [
     StarkInducedCZGate,
     CZGate,
 ]
-=======
-from quam_builder.architecture.superconducting.custom_gates.flux_tunable_transmon_pair.two_qubit_gates import CZGate
-
-__all__ = [
-    CZGate,
-]
->>>>>>> main
