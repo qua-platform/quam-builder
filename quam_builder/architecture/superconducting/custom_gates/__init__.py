a9a69b600aeee042325c97f40fb9893741344983
TheoLaudatQM <98808790+TheoLaudatQM@users.noreply.github.com>
2025-10-27 11:24:59 +0100

    Feat/gate fidelity (#21)
    * Add fidelity and extras and dict
    
    * changelog

--- quam_builder/architecture/superconducting/custom_gates/__init__.py
+++ quam_builder/architecture/superconducting/custom_gates/__init__.py
@@ -1,4 +1,6 @@
-from quam_builder.architecture.superconducting.custom_gates.flux_tunable_transmon_pair.two_qubit_gates import CZGate
+from quam_builder.architecture.superconducting.custom_gates.flux_tunable_transmon_pair.two_qubit_gates import (
+    CZGate,
+)
 
 __all__ = [
     CZGate,
