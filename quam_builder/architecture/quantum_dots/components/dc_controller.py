f6350dc01fcb1f82f1542527472eca1f814fb760
TorbjornRasmussenQM <torbjorn.rasmussen@quantum-machines.co>
2025-11-06 20:47:32 +0100

    dc controller initial push

--- quam_builder/architecture/quantum_dots/components/dc_controller.py
+++ quam_builder/architecture/quantum_dots/components/dc_controller.py
@@ -11,22 +11,27 @@ class DCController(QuamComponent):
 
     def __post_init__(self):
         self.num_layers = len(self.virtual_gate_set.layers)
-        self.dc_state = self.get_dc()
+        self.dc_state = self._get_dc()
         self.virtual_state = self._inverse_resolve(self.dc_state)
 
     def set_dc(self, voltages: Dict[str, VoltageLevelType]):
+        # updates internal state to reflect new set virtual dc value
         self.virtual_state.update(voltages)
 
         resolved_voltages = self.virtual_gate_set.resolve_voltages(
             voltages=self.virtual_state
         )
 
+        self.dc_state.update(resolved_voltages)
         for VoltageGate, value in resolved_voltages.items():
             self.virtual_gate_set.channels[VoltageGate].offset_parameter = value
 
-    def get_dc(
+    def _get_dc(
         self,
     ):
+        """
+        Queries all VoltageGate.offset_parameters for a value.
+        """
         dc_values = {
             ch_name: ch.offset_parameter
             for ch_name, ch in self.virtual_gate_set.channels.items()
@@ -35,7 +40,7 @@ class DCController(QuamComponent):
 
     def _inverse_resolve(self, dc_values):
         """
-        from dc to virtual layers
+        Inverse resolve of dc values to virtual gate layers
         """
         resolved_virtual = {}
         first_layer = self._resolve_layer(
@@ -49,7 +54,7 @@ class DCController(QuamComponent):
         resolved_virtual.update(first_layer)
 
         if self.num_layers > 1:
-            for layer in self.virtual_gate_set.layers[1:]:
+            for layer in list(self.virtual_gate_set.layers)[1:]:
                 layer_adds = self._resolve_layer(
                     layer,
                     [
