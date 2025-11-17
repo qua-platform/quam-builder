fa86c2c3b6c78261c7b98b3c6b4c2e0b076dee6f
paulQM <paul.jamet@quantum-machines.co>
2025-11-17 23:19:47 +0100

    fix

--- quam_builder/architecture/superconducting/components/reset_drive.py
+++ quam_builder/architecture/superconducting/components/reset_drive.py
@@ -1,34 +1,15 @@
 from quam.components.channels import IQChannel, MWChannel
 from quam.core import quam_dataclass
+from quam_builder.architecture.superconducting.components.xy_drive import XYDriveMW, XYDriveIQ
 
 __all__ = ["ResetIQ", "ResetMW"]
 
 
 @quam_dataclass
-class ResetBase:
-    target_qubit_LO_frequency: int = None
-    target_qubit_IF_frequency: int = None
-    detuning: int = None
+class ResetIQ(XYDriveIQ):
+    pass
 
 
 @quam_dataclass
-class ResetIQ(IQChannel, ResetBase):
-
-    @property
-    def upconverter_frequency(self):
-        return self.LO_frequency
-
-    @property
-    def inferred_intermediate_frequency(self):
-        return self.target_qubit_LO_frequency + self.target_qubit_IF_frequency - self.LO_frequency + self.detuning
-
-
-@quam_dataclass
-class ResetMW(MWChannel, ResetBase):
-    @property
-    def inferred_intermediate_frequency(self):
-        return self.target_qubit_LO_frequency + self.target_qubit_IF_frequency - self.LO_frequency + self.detuning
-
-    @property
-    def upconverter_frequency(self):
-        return self.opx_output.upconverter_frequency
+class ResetMW(XYDriveMW):
+    pass
