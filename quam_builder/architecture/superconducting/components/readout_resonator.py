b1edf2039fef8d296d600e5567fa71f9e3c7d2b5
omrieoqm <omrie.ovdat@quantum-machines.co>
2025-12-24 13:20:18 +0200

    measurment twirl init code

--- quam_builder/architecture/superconducting/components/readout_resonator.py
+++ quam_builder/architecture/superconducting/components/readout_resonator.py
@@ -117,6 +117,7 @@ class ReadoutResonatorIQ(InOutIQChannel, ReadoutResonatorBase):
 @quam_dataclass
 class ReadoutResonatorMW(InOutMWChannel, ReadoutResonatorBase):
     intermediate_frequency: int = "#./inferred_intermediate_frequency"
+    twirl: bool = False
 
     @property
     def upconverter_frequency(self):
@@ -159,3 +160,16 @@ class ReadoutResonatorMW(InOutMWChannel, ReadoutResonatorBase):
         return set_output_power_mw_channel(
             self, power_in_dbm, operation, full_scale_power_dbm, max_amplitude
         )
+        
+    # def measure(
+    #     self,
+    #     pulse_name: str,
+    #     amplitude_scale: Optional[Union[ScalarFloat, Sequence[ScalarFloat]]] = None,
+    #     qua_vars: Tuple[QuaVariableFloat, QuaVariableFloat] = None,
+    #     stream=None,
+    # ) -> Tuple[QuaVariableFloat, QuaVariableFloat]
+        
+        
+    #     # measurment twirl logic
+        
+    #     super("InOutMWChannel").super("_InOutMWChannel").measure()
