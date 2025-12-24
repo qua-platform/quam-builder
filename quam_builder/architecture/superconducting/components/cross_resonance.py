b6802227ac0d0c3351e92872e86a754bd7445ff6
soonqm <soon.teh@quantum-machines.co>
2025-12-24 15:26:42 +0900

    default cancel_amplitude_scaling=1

--- quam_builder/architecture/superconducting/components/cross_resonance.py
+++ quam_builder/architecture/superconducting/components/cross_resonance.py
@@ -16,7 +16,7 @@ class CrossResonanceBase:
     target_qubit_RF_frequency: float = None
     drive_amplitude_scaling: float = 1.0
     drive_phase: float = 0.0
-    cancel_amplitude_scaling: float = 0.0
+    cancel_amplitude_scaling: float = 1.0
     cancel_phase: float = 0.0
 
     qc_correction_phase: float = 0.0
