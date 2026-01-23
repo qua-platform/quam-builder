49c789415fed0b35b7ec683a33dec6e38171199f
KU-QM <kalid.ulas@quantum-machines.co>
2026-01-23 11:30:33 +0000

    Quick fix for load classmethod

--- quam_builder/architecture/quantum_dots/qpu/base_quam_qd.py
+++ quam_builder/architecture/quantum_dots/qpu/base_quam_qd.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Union, ClassVar, Optional, Literal, Tuple, Callab
 from dataclasses import field
 import numpy as np
 from collections import defaultdict
+from pathlib import Path
 from quam.core.macro import QuamMacro
 from qm import QuantumMachinesManager, QuantumMachine
 from qm.octave import QmOctaveConfig
@@ -718,11 +719,20 @@ class BaseQuamQD(QuamRoot):
         # All the relevant information about the sequence (points, macros) are stored on the QuantumDot/Qubit level and/or the VirtualGateSet level.
         d.pop("voltage_sequences", None)
         return d
-
+    
     @classmethod
-    def load(cls, filepath, *args, **kwargs):
+    def load(
+        cls, 
+        filepath_or_dict: Optional[Union[str, Path, dict]] = None, 
+        validate_type: bool = True, 
+        fix_attrs: bool = True,
+    ): 
         """Load machine from file and recreate voltage sequences"""
-        instance = super().load(filepath, *args, **kwargs)
+        instance = super().load(
+            filepath_or_dict=filepath_or_dict, 
+            validate_type=validate_type, 
+            fix_attrs=fix_attrs,
+        )
         instance.voltage_sequences = {}
 
         # Recreate voltage sequences for each virtual gate set
