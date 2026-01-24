ab7b59e4d5eb54a1e4a1cae1ce1f719d1b8cef1a
MichalGoldenshtein <michal@quantum-machines.co>
2026-01-23 18:01:21 -0600

    add modifications

---
+++ quam_builder/architecture/superconducting/qubit/cavity.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from quam.core import quam_dataclass
+
+from quam_builder.architecture.superconducting.qubit.flux_tunable_transmon import (
+    FluxTunableTransmon,
+)
+from quam_builder.architecture.superconducting.components.xy_drive import XYDriveIQ, XYDriveMW
+
+
+__all__ = ["FluxTunableTransmonSideBand"]
+
+
+
+
+@quam_dataclass
+class FluxTunableTransmonSideBand(FluxTunableTransmon):
+    """
+    Example QUAM component for a flux tunable transmon qubit.
+
+    Args:
+
+    """
+    sideband1: Union[XYDriveIQ, XYDriveMW] = None
+    sideband2: Union[XYDriveIQ, XYDriveMW] = None
