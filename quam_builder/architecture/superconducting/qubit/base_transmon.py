2da3a29708c0edbc40a47c9cf56228362c013cd5
omrieoqm <omrie.ovdat@quantum-machines.co>
2026-02-09 13:03:39 +0200

    Update version to 0.2.2 in pyproject.toml, remove unused twirl attributes from ReadoutResonatorMW and BaseTransmon classes, and clean up CZGate class by removing commented-out code for randomized compiling.

--- quam_builder/architecture/superconducting/qubit/base_transmon.py
+++ quam_builder/architecture/superconducting/qubit/base_transmon.py
@@ -28,7 +28,6 @@ from qm.qua import (
     update_frequency,
     Math,
     Cast,
-    Random
 )
 
 
@@ -94,9 +93,6 @@ class BaseTransmon(Qubit):
     chi: float = None
     grid_location: str = None
     gate_fidelity: Dict[str, Any] = field(default_factory=dict)
-    
-    twirl_measurement: bool = False
-    
     extras: Dict[str, Any] = field(default_factory=dict)
 
     @property
@@ -227,23 +223,8 @@ class BaseTransmon(Qubit):
         Q = declare(fixed)
         if threshold is None:
             threshold = self.resonator.operations[pulse_name].threshold
-        
-        do_flip = declare(int)
-        if self.twirl_measurement:
-            assign(do_flip, Random().rand_int(2))
-            with if_(do_flip == 1, unsafe=True):
-                self.xy.play("x180")
-            self.align()
-
         self.resonator.measure(pulse_name, qua_vars=(I, Q))
-
-        if self.twirl_measurement:
-            assign(state, Cast.to_int(I > threshold))
-            with if_(do_flip == 1, unsafe=True):
-                assign(state, 1- state)
-        else:
-            assign(state, Cast.to_int(I > threshold))
-
+        assign(state, Cast.to_int(I > threshold))
         wait(self.resonator.depletion_time // 4, self.resonator.name)
 
     def reset(
@@ -449,4 +430,3 @@ class BaseTransmon(Qubit):
         """
         channel_names = [channel.name for channel in self.channels.values()]
         wait(duration, *channel_names)
-
