90836845f2a6d555e8cc2f7bb1e561b3b8bcc29e
MichalGoldenshtein <michal@quantum-machines.co>
2026-02-25 08:59:25 -0600

    delete a copy of FluxTunableTransmon in quam_builder/architecture/superconducting/qubit/flux_tunable_transmon.py

--- quam_builder/architecture/superconducting/qubit/flux_tunable_transmon.py
+++ quam_builder/architecture/superconducting/qubit/flux_tunable_transmon.py
@@ -9,26 +9,6 @@ from quam_builder.architecture.superconducting.components.flux_line import FluxL
 
 __all__ = ["FluxTunableTransmon"]
 
-
-@quam_dataclass
-class FluxTunableTransmon(FixedFrequencyTransmon):
-    """
-    Example QUAM component for a flux tunable transmon qubit.
-
-    Args:
-        z (FluxLine): The z drive component.
-        resonator (ReadoutResonator): The readout resonator component.
-        freq_vs_flux_01_quad_term (float): Quadratic term of the qubit frequency versus flux parabola.
-        phi0_current (float): The qubit flux quantum in Ampere.
-        phi0_voltage (float): The qubit flux quantum in Volt.
-    """
-
-    z: FluxLine = None
-    freq_vs_flux_01_quad_term: float = 0.0
-    phi0_current: float = 0.0
-    phi0_voltage: float = 0.0
-
-
 @quam_dataclass
 class FluxTunableTransmon(FixedFrequencyTransmon):
     """
