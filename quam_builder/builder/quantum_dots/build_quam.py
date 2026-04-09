6f87f3ea210c91e85a37c5c63d141a68887effe3
TheoQM <theo@quantum-machines.co>
2026-04-09 12:35:54 +0200

    fix QUAM reference warning

--- quam_builder/builder/quantum_dots/build_quam.py
+++ quam_builder/builder/quantum_dots/build_quam.py
@@ -686,5 +686,4 @@ def add_dacs(
                 with_trigger_channel=bool(entry.get("trigger", False)),
                 digital_output_key=digital_output_key,
                 qdac_trigger_in=entry_qti,
-                trigger_pulse_length_ns=trigger_pulse_length_ns,
-            )
+remove trigger pulse length            )
