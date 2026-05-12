130e7598fe1307938d9853405c667fc8462e0122
TorbjornRasmussenQM <torbjorn.rasmussen@quantum-machines.co>
2026-05-12 17:00:02 +0200

    almost fixed, need to validate latency

--- tests_against_server/voltage_gate_sequence/test_attenuation_amplified.py
+++ tests_against_server/voltage_gate_sequence/test_attenuation_amplified.py
@@ -7,7 +7,7 @@ from qm import qua
 
 def test_square_pulses_amplified_with_attenuation(qmm, machine_amplified: QuamGateSet):
     """Physical gate voltages match simulation when adjust_for_attenuation is on and ports are amplified."""
-    level_init = [0.75, -0.1]
+    level_init = [0.3, -0.1]
     duration_init = 1000
     level_manip = [0.5, -0.3]
     duration_manip = 100
@@ -24,6 +24,12 @@ def test_square_pulses_amplified_with_attenuation(qmm, machine_amplified: QuamGa
         for i in range(2)
     ]
 
+    atten_p = -machine_amplified.gate_set.channels["ch1"].attenuation
+    atten_m = -machine_amplified.gate_set.channels["ch2"].attenuation
+    attenuation_factor_p = 10 ** (atten_p / 20)
+    attenuation_factor_m = 10 ** (atten_m / 20)
+    requested_wf_p = [x / attenuation_factor_p for x in requested_wf_p]
+    requested_wf_m = [x / attenuation_factor_m for x in requested_wf_m]
     machine_amplified.gate_set.add_point(
         "initialization",
         voltages={"ch1": level_init[0], "ch2": level_init[1]},
@@ -53,7 +59,7 @@ def test_square_pulses_amplified_with_attenuation(qmm, machine_amplified: QuamGa
 
 def test_square_pulses_amplified_with_attenuation_qua(qmm, machine_amplified: QuamGateSet):
     """Same as test_square_pulses_amplified_with_attenuation but first plateau uses QUA fixed variables."""
-    level_init = [0.75, -0.1]
+    level_init = [0.3, -0.1]
     duration_init = 1000
     level_manip = [0.5, -0.3]
     duration_manip = 100
@@ -85,6 +91,12 @@ def test_square_pulses_amplified_with_attenuation_qua(qmm, machine_amplified: Qu
         )
         for i in range(2)
     ]
+    atten_p = -machine_amplified.gate_set.channels["ch1"].attenuation
+    atten_m = -machine_amplified.gate_set.channels["ch2"].attenuation
+    attenuation_factor_p = 10 ** (atten_p / 20)
+    attenuation_factor_m = 10 ** (atten_m / 20)
+    requested_wf_p = [x / attenuation_factor_p for x in requested_wf_p]
+    requested_wf_m = [x / attenuation_factor_m for x in requested_wf_m]
 
     with qua.program() as prog:
         a = qua.declare(qua.fixed, value=level_init[0])
